69472b27bb2563ab206ce706831220866837f126
yilin <yilinzh54@gmail.com>
2025-07-07 08:38:18 +0900

    Done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,12 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Finds the next number of a sequence given the previous number */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n%2 == 0  ) {
+            return n/2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
 
