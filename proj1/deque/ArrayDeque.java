70f547eafee1ab148087d348d19c70f0676d6efa
yilin <yilinzh54@gmail.com>
2025-08-17 17:41:43 +0800

    fix whitespaces

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,14 +1,14 @@
 package deque;
 import java.util.Iterator;
 
-public class ArrayDeque<T> implements Deque<T>{
+public class ArrayDeque<T> implements Deque<T> {
     int arraySize;
     T[] array;
     int nextFirst;
     int nextLast;
     int size;
 
-    public ArrayDeque () {
+    public ArrayDeque() {
         arraySize = 8;
         size = 0;
         array = (T[]) new Object[8];
@@ -17,14 +17,14 @@ public class ArrayDeque<T> implements Deque<T>{
     }
 
     @Override
-    public T removeFirst () {
+    public T removeFirst() {
         int removeIndex = nextFirst + 1;
         T removedItem = null;
         if (nextFirst == arraySize - 1) {
             removeIndex = 0;
         }
         if (size != 0) {
-            size -- ;
+            size--;
             removedItem = array[removeIndex];
             nextFirst = removeIndex;
         }
@@ -34,12 +34,12 @@ public class ArrayDeque<T> implements Deque<T>{
     //returns the x's item of the array
 
     @Override
-    public T get (int index) {
+    public T get(int index) {
         int arrayIndex = nextFirst;
-        for(int i = 0; i <= index; i++){
-            if(arrayIndex == arraySize-1){
-                arrayIndex=0;
-            }else{
+        for (int i = 0; i <= index; i++) {
+            if (arrayIndex == arraySize - 1) {
+                arrayIndex = 0;
+            } else {
                 arrayIndex++;
             }
         }
@@ -48,14 +48,14 @@ public class ArrayDeque<T> implements Deque<T>{
 
     //removes and returns the last item in the list
     @Override
-    public T removeLast(){
-        int removeIndex = nextLast-1;
+    public T removeLast() {
+        int removeIndex = nextLast - 1;
         T removedItem = null;
-        if (nextLast==0){
-            removeIndex = arraySize-1;
+        if (nextLast == 0) {
+            removeIndex = arraySize - 1;
         }
-        if (size != 0){
-            size --;
+        if (size != 0) {
+            size--;
             removedItem = array[removeIndex];
             nextLast = removeIndex;
         }
@@ -64,24 +64,24 @@ public class ArrayDeque<T> implements Deque<T>{
 
     //returns the number of elements in the array
     @Override
-    public int size(){
+    public int size() {
         return size;
     }
 
     //helper function to check if a resize is necessary when adding elements
-    private void checkIncSize(){
-        if (arraySize == size){
+    private void checkIncSize() {
+        if (arraySize == size) {
             resize();
         }
     }
 
     //adds the parameter to the front of the array
     @Override
-    public void addFirst(T x){
+    public void addFirst(T x) {
         checkIncSize();
         size += 1;
         array[nextFirst] = x;
-        if (nextFirst == 0){
+        if (nextFirst == 0) {
             nextFirst = arraySize - 1;
         } else {
         nextFirst -= 1;}
@@ -89,56 +89,56 @@ public class ArrayDeque<T> implements Deque<T>{
 
     //adds the parameter to the back of the array
     @Override
-    public void addLast(T x){
+    public void addLast(T x) {
         checkIncSize();
         size += 1;
         array[nextLast] = x;
-        if (nextLast == arraySize-1){
+        if (nextLast == arraySize - 1) {
             nextLast = 0;
-        }else{
+        } else {
         nextLast += 1;}
     }
 
     //resizes the array up or down
-    private void resize(){
+    private void resize() {
         int copyIndex;
-        if (nextFirst == size-1){
+        if (nextFirst == size - 1) {
             copyIndex = 0;
-        }else{
-            copyIndex= nextFirst+1;
+        } else {
+            copyIndex = nextFirst + 1;
         }
 
-        if (size< arraySize / 4){
+        if (size < arraySize / 4) {
             T[] newDecArray = (T[]) new Object[arraySize / 4];
-            resizeHelper(array, newDecArray, copyIndex , size);
-            array= newDecArray;
-            arraySize= arraySize / 4;
-        }else{
-            T[] newIncArray = (T[]) new Object[arraySize*2];
+            resizeHelper(array, newDecArray, copyIndex, size);
+            array = newDecArray;
+            arraySize = arraySize / 4;
+        } else {
+            T[] newIncArray = (T[]) new Object[arraySize * 2];
             resizeHelper(array, newIncArray, copyIndex, size);
             array = newIncArray;
-            arraySize = arraySize*2;
+            arraySize = arraySize * 2;
         }
-        nextFirst = arraySize-1;
+        nextFirst = arraySize - 1;
         nextLast = size;
 
     }
 
     //helper function for resize - custom copy array func
-    private void resizeHelper(T[] source, T[] destination, int startIndexSource, int length){
-        for(int i = 0 ; i <length; i++){
-            if (startIndexSource == arraySize){
-                startIndexSource=0;
+    private void resizeHelper(T[] source, T[] destination, int startIndexSource, int length) {
+        for(int i = 0 ;i < length; i++){
+            if (startIndexSource == arraySize) {
+                startIndexSource = 0;
             }
             destination[i] = source[startIndexSource];
-            startIndexSource ++;
+            startIndexSource++;
         }
     }
 
     @Override
-    public void printDeque(){
+    public void printDeque() {
         String toPrint = "";
-        for (int i = 0; i<size; i++){
+        for (int i = 0; i< size; i++){
             toPrint += get(i) + " ";
         }
         System.out.println(toPrint);
@@ -166,16 +166,16 @@ public class ArrayDeque<T> implements Deque<T>{
         return false;
     }
 
-    private class ArrayDequeIterator implements Iterator<T>{
+    private class ArrayDequeIterator implements Iterator<T> {
         int wizPos;
 
-        public ArrayDequeIterator(){
+        public ArrayDequeIterator() {
             wizPos = 0;
         }
 
         @Override
         public boolean hasNext() {
-            if (wizPos < size){
+            if (wizPos < size) {
                 return true;
             }
             return false;
@@ -189,7 +189,7 @@ public class ArrayDeque<T> implements Deque<T>{
         }}
 
     @Override
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new ArrayDequeIterator();
     }
 }
