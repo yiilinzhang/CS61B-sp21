dfc203e30dded6f1a84f34c27073f9ab6d11e6b5
yilin <yilinzh54@gmail.com>
2025-08-17 17:50:12 +0800

    fixed whitespace

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -20,9 +20,9 @@ public class LinkedListDeque<T> implements Deque<T> {
     private Node getHelper;
 
     //Initialises the first Linkedlist
-    public LinkedListDeque(){
-        size=0;
-        sentinel= new Node(null, null, null);
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
         getHelper = sentinel;
@@ -30,17 +30,17 @@ public class LinkedListDeque<T> implements Deque<T> {
 
     //Adds an item of type T to the front of the deque. You can assume that item is never null.
     @Override
-    public void addFirst(T item){
-        size +=1;
+    public void addFirst(T item) {
+        size += 1;
         Node newNode = new Node(item, sentinel, sentinel.next);
-        sentinel.next.prev= newNode;
+        sentinel.next.prev = newNode;
         sentinel.next = newNode;
     }
 
     //Adds an item of type T to the back of the deque. You can assume that item is never null.
     @Override
-    public void addLast(T item){
-        size+=1;
+    public void addLast(T item) {
+        size += 1;
         Node newNode = new Node(item, sentinel.prev, sentinel);
         sentinel.prev.next = newNode;
         sentinel.prev = newNode;
@@ -50,17 +50,17 @@ public class LinkedListDeque<T> implements Deque<T> {
 
     //Returns the number of items in the deque.
     @Override
-    public int size(){return size;}
+    public int size() {return size;}
 
     /*Prints the items in the deque from first to last, separated by a space.
     Once all the items have been printed, print out a new line.
      */
     @Override
-    public void printDeque(){
-        Node tempPointer= sentinel;
+    public void printDeque() {
+        Node tempPointer = sentinel;
         String toPrint = "";
-        for (int i = 0; i<size; i++){
-            tempPointer= tempPointer.next;
+        for (int i = 0; i < size; i++) {
+            tempPointer = tempPointer.next;
             toPrint += tempPointer.item + " ";
         }
         System.out.println(toPrint);
@@ -68,35 +68,35 @@ public class LinkedListDeque<T> implements Deque<T> {
 
     //Removes and returns the item at the front of the deque. If no such item exists, returns null.
     @Override
-    public T removeFirst(){
-        if(size ==0){
+    public T removeFirst() {
+        if(size == 0){
             return null;
         }
-        size -=1;
+        size -= 1;
         Node firstItem = sentinel.next;
         sentinel.next = firstItem.next;
-        firstItem.next.prev=sentinel;
+        firstItem.next.prev = sentinel;
         return firstItem.item;
     };
 
     //Removes and returns the item at the back of the deque. If no such item exists, returns null.
     @Override
-    public T removeLast(){
-        if(size ==0){
+    public T removeLast() {
+        if (size == 0) {
             return null;
         }
-        size -=1;
+        size -= 1;
         Node lastItem = sentinel.prev;
-        sentinel.prev=lastItem.prev;
-        lastItem.prev.next=sentinel;
+        sentinel.prev = lastItem.prev;
+        lastItem.prev.next = sentinel;
         return lastItem.item;
     }
 
     //Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!
     @Override
-    public T get(int index){
+    public T get(int index) {
         Node tempPointer = sentinel;
-        for(int i = 0 ; i<=index; i++){
+        for (int i = 0 ; i <= index; i++) {
             tempPointer = tempPointer.next;
         }
         return tempPointer.item;
@@ -117,13 +117,13 @@ public class LinkedListDeque<T> implements Deque<T> {
     private class ListDequeIterator implements Iterator<T> {
         int wizPos;
 
-        public ListDequeIterator(){
+        public ListDequeIterator() {
             wizPos = 0;
         }
 
         @Override
         public boolean hasNext() {
-            if (wizPos < size){
+            if (wizPos < size) {
                 return true;
             }
             return false;
--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,11 +1,8 @@
 package gh2;
 
-// TODO: uncomment the following import once you're ready to start this portion
 import deque.Deque;
 import deque.LinkedListDeque;
 
-import java.util.Random;
-// TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -16,19 +13,13 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
     private Deque<Double> buffer;
-    private double frontData;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
-        int capacity = (int) Math.round(SR/frequency);
+        int capacity = (int) Math.round(SR / frequency);
         buffer = new LinkedListDeque<>();
-        for (int x = 0 ; x <capacity ; x ++){
+        for (int x = 0 ; x < capacity; x++){
             buffer.addLast(0.0);
         }
 
@@ -37,15 +28,11 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
-        for ( int i =0 ; i<buffer.size() ; i ++){
+        for ( int i = 0; i < buffer.size(); i++) {
             buffer.addFirst(Math.random() - 0.5);
             buffer.removeLast();
         }
@@ -55,17 +42,12 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
-        buffer.addLast((buffer.get(0) + buffer.get(1)) * 0.5* 0.996);
+        buffer.addLast((buffer.get(0) + buffer.get(1)) * 0.5 * 0.996);
         buffer.removeFirst();
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
