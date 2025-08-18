2e19e2e5ef4908a316e12fce96a0190a3cb853eb
yilin <yilinzh54@gmail.com>
2025-08-18 12:51:09 +0800

    fixed bug on equality

---
+++ lab4/flik/FlikTest.java
@@ -0,0 +1,11 @@
+package flik;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlikTest {
+    @Test
+    public void equalityTest() {
+        boolean result = Flik.isSameNumber(129, 129);
+    }
+}
